a0bf01a2462dec02b14c65bc7c49e984f1a8e6d3
TimSVector <tim.schneider@vector.com>
2025-11-23 17:11:14 -0500

    updates for Jenkins 2.538

--- src/main/java/com/vectorcast/plugins/vectorcastcoverage/VectorCASTPublisher.java
+++ src/main/java/com/vectorcast/plugins/vectorcastcoverage/VectorCASTPublisher.java
@@ -51,10 +51,10 @@ public class VectorCASTPublisher extends Recorder implements SimpleBuildStep {
     /**
      * Relative path to the VectorCAST XML file inside the workspace.
      */
-    public String includes;
-    public Boolean useThreshold;
-    public Boolean useCoverageHistory;
-    public Integer maxHistory;
+    private String includes;
+    private Boolean useThreshold;
+    private Boolean useCoverageHistory;
+    private Integer maxHistory;
     
     /**
     /**
@@ -62,16 +62,16 @@ public class VectorCASTPublisher extends Recorder implements SimpleBuildStep {
      *
      * TODO: define a configuration mechanism.
      */
-    public Rule rule;
+    private Rule rule;
 
     /**
      * {@link hudson.model.HealthReport} thresholds to apply.
      */
-    public VectorCASTHealthReportThresholds healthReports = new VectorCASTHealthReportThresholds(0, 100, 0, 70, 0, 80, 0, 80, 0, 80, 0, 80 );
+    private VectorCASTHealthReportThresholds healthReports = new VectorCASTHealthReportThresholds(0, 100, 0, 70, 0, 80, 0, 80, 0, 80, 0, 80 );
 
     // should not be used
-    public VectorCASTHealthReportThresholds healthyTarget;
-    public VectorCASTHealthReportThresholds unhealthyTarget = null;
+    private VectorCASTHealthReportThresholds healthyTarget;
+    private VectorCASTHealthReportThresholds unhealthyTarget = null;
     private static final Logger logger = Logger.getLogger(VectorCASTPublisher.class.getName());
 
     public VectorCASTPublisher() {
@@ -235,7 +235,6 @@ public class VectorCASTPublisher extends Recorder implements SimpleBuildStep {
         performImpl(run, workspace, listener);
     }
 
-    @SuppressFBWarnings(value = "DCN_NULLPOINTER_EXCEPTION", justification = "TODO needs triage")
     public boolean performImpl(Run<?, ?> run, FilePath workspace, TaskListener listener) throws InterruptedException, IOException {
         final PrintStream logger = listener.getLogger();
         
@@ -558,9 +557,7 @@ public class VectorCASTPublisher extends Recorder implements SimpleBuildStep {
         logger.println("[VectorCASTCoverage] [FAIL]: " + coverageType + " coverage " + percent +"% < " + threshold + "% threshold.");
     }
     
-    
-	@SuppressFBWarnings(value = "DCN_NULLPOINTER_EXCEPTION", justification = "TODO needs triage")
-	private void checkThreshold(Run<?, ?> run,
+    private void checkThreshold(Run<?, ?> run,
 		final PrintStream logger, final VectorCASTBuildAction action) {
 			
 		if (useThreshold && unhealthyTarget == null) {
